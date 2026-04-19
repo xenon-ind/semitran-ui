9c5afd200b5d0c37ecb23264c244c1ed3e47fca4
SeniorShadifer <discordeg@mail.ru>
2026-04-19 09:37:14 +0300

    build(vite): switch vite to library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
